116179179d266487402f6b00b1ac2bb9b8820cfd
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2020-09-10 18:07:51 -0400

    update

--- scripts/python_scripts/tune_settings.py
+++ scripts/python_scripts/tune_settings.py
@@ -14,14 +14,17 @@ def main():
     dataset, build_folder = sys.argv[1], sys.argv[2]
 
     suggested_eps = map(str, [-3, -6])
-    suggested_max_iter = map(str, [0, 1, 2, 3, 4, 5, 10, 20])
-    suggested_stop_tol = map(str, [-13, -15, -16, -17])
-    suggested_diag_perturb = [-6, -7, -8, -9, -10, -11, -12]
+    suggested_max_iter = map(str, [0, 5, 10])
+    suggested_stop_tol = map(str, [-13, -17])
+    suggested_diag_perturb = [-6, -9, -12]
 
     for eps in suggested_eps:
         for max_iter in suggested_max_iter:
             for stop_tol in suggested_stop_tol:
                 for diag_perturb in suggested_diag_perturb:
+                    call(["rm", "-f", "*.png"])
+                    call(["rm", "-f", "*.cvs"])
+
                     os.makedirs("../../setting_plots/eps{}_max_iter{}_stop_tol{}_diag_perturb{}".format(eps, max_iter, stop_tol, diag_perturb))
 
                     call(["echo", "Running NASOQ-Fixed ..."])
@@ -36,8 +39,8 @@ def main():
                     call(["bash", "../NASOQ_bench.sh", "../../{}/nasoq/NASOQ-BIN".format(build_folder), dataset, eps, \
                         "-p {} -r {} -t {} -v predet".format(diag_perturb, max_iter, stop_tol), ">", "../../logs/nasoq-custom-e{}.csv".format(eps)])
 
-                    call(["python", "graph_generator.py", "-d ../../logs/ -s", eps, ">", \
-                        "../../setting_plots/eps{}_max_iter{}_stop_tol{}_diag_perturb{}".format(eps, max_iter, stop_tol, diag_perturb)])
+                    call(["python", "graph_generator.py", "-d ../../logs/ -s", eps])
+                    call(["mv", "*.png", "../../setting_plots/eps{}_max_iter{}_stop_tol{}_diag_perturb{}".format(eps, max_iter, stop_tol, diag_perturb)])
 
 if __name__ == "__main__":
     # for i in range(10):
