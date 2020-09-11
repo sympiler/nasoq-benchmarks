c5d9091fdca10c30a3e8eb3cbe4ff6e093d49b88
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2020-09-11 02:47:52 -0400

    debug

--- scripts/python_scripts/tune_settings.py
+++ scripts/python_scripts/tune_settings.py
@@ -18,8 +18,8 @@ def tune(eps, diag_perturb, max_iter, stop_tol, build_folder, dataset):
     os.system("rm -f *.png")
 
     call(["echo", "Running NASOQ-Fixed ..."])
-    os.system("bash scripts/NASOQ_bench.sh {}/nasoq/NASOQ-BIN".format(build_folder) + " {} ".format(dataset) + eps + \
-        " -p {} -r {} -t {}".format(diag_perturb, max_iter, stop_tol) + ">" + "logs/nasoq-fixed-e{}.csv".format(eps))
+    # os.system("bash scripts/NASOQ_bench.sh {}/nasoq/NASOQ-BIN".format(build_folder) + " {} ".format(dataset) + eps + \
+    #     " -p {} -r {} -t {}".format(diag_perturb, max_iter, stop_tol) + ">" + "logs/nasoq-fixed-e{}.csv".format(eps))
 
     call(["echo", "Running NASOQ-Tuned ..."])
     call(["bash","scripts/NASOQ_bench.sh", "{}/nasoq/NASOQ-BIN".format(build_folder), "{}".format(dataset), eps, \
