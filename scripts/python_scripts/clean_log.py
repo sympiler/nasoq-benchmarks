3b5108409e5b92f1f57716d9adb2b10fbbed6b97
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2020-10-26 01:55:17 -0400

    add comments

--- scripts/python_scripts/clean_log.py
+++ scripts/python_scripts/clean_log.py
@@ -10,13 +10,19 @@ diag_perturb_labels = {"diag_perturb-10": -10, "diag_perturb-9": -9, "diag_pertu
 eps_labels = {"eps-3": -3, "eps-6": -6}
 
 def compute_metrics(analysis_path):
+    """
+    compute and process the metrics of each QP
+    """
     def compute_numerical_range(cell1, cell2):
+        """
+        compute numerical range: abs(max / min)
+        """
         result = 0.0
         if cell2 != 0.0:
             result = np.abs(cell1 / cell2)
         return np.float64(result)
 
-
+    # read out the analysis data and process the data
     df = pd.read_csv(analysis_path)
     df["Problem name"] = df["Problem name"].str.lower()
 
@@ -32,6 +38,7 @@ def compute_metrics(analysis_path):
     df.loc[df["equality max"] == "\\", "equality max"] = 0
     df.loc[df["equality min"] == "\\", "equality min"] = 0
 
+    # compute the number of constraints: equality constr # + inequality constr #
     df["number of constraints"] = np.int32(df["Number of equality constraints"].values) + np.int32(df["Number of inequality constraints"].values)
     df["QP max"] = np.float64(df["QP max"])
     df["inequality max"] = np.float64(df["inequality max"])
@@ -40,6 +47,7 @@ def compute_metrics(analysis_path):
     df["inequality min"] = np.float64(df["inequality min"])
     df["equality min"] = np.float64(df["equality min"])
 
+    # compute the numerical range
     df["objective numerical range (abs(max / eps-3))"] = np.abs(df["QP max"]) / 1e-3
     df["inequality numerical range (abs(max / eps-3))"] = np.abs(df["inequality max"]) / 1e-3
     df["equality numerical range (abs(max / eps-3))"] = np.abs(df["equality max"]) / 1e-3
@@ -61,13 +69,18 @@ def compute_metrics(analysis_path):
                             "inequality numerical range (abs(max / min))", "equality numerical range (abs(max / min))"]]
 
 def get_status(perf_path, eps="eps-3"):
-    print(eps)
+    """
+    get the a dataframe of status for each diag_perturb and max_iter
+    and select rows with any failure
+    """
+    # print(eps)
     diag_perturb_data = os.listdir(perf_path + "diag_perturb/")
     max_iter_data = os.listdir(perf_path + "max_iter/")
 
     diag_perturb_df_dict = {}
     max_iter_df_dict = {}
 
+    # filter the status from diag_perturb dataframes
     for label in diag_perturb_labels.keys():
         for datum in diag_perturb_data:
             if datum.endswith(".csv") and label in datum and label + '0' not in datum and eps in datum:
@@ -78,6 +91,7 @@ def get_status(perf_path, eps="eps-3"):
                 # print(diag_perturb_df_dict[label]['{} status'.format(label)][0])
                 diag_perturb_df_dict[label]['{} status'.format(label)] = np.int32(diag_perturb_df_dict[label]['{} status'.format(label)])
     
+    # filter the status from max_iter dataframes
     for label in max_iter_labels.keys():
         for datum in max_iter_data:
             if datum.endswith(".csv") and label in datum and label + '0' not in datum and eps in datum:
@@ -93,11 +107,13 @@ def get_status(perf_path, eps="eps-3"):
     diag_perturb_data_lst = list(diag_perturb_df_dict.values())
     max_iter_data_lst = list(max_iter_df_dict.values())
 
+    # merge all datasets of each status
     for df_m in diag_perturb_data_lst:
         df = df.merge(df_m, on="Problem name", how="inner")
     for df_m in max_iter_data_lst:
         df = df.merge(df_m, on="Problem name", how="inner")
     
+    # drop the row which has no failure
     df.drop(df[(df['max_iter0 status'] == 1) & (df['max_iter1 status'] == 1) & (df['max_iter2 status'] == 1) & (df['max_iter3 status'] == 1) & \
         (df['max_iter4 status'] == 1) & (df['max_iter5 status'] == 1) & (df['max_iter10 status'] == 1) & (df['max_iter20 status'] == 1) & \
             (df['diag_perturb-10 status'] == 1) & (df['diag_perturb-9 status'] == 1) & (df['diag_perturb-8 status'] == 1) & \
@@ -106,6 +122,9 @@ def get_status(perf_path, eps="eps-3"):
     return df
 
 def df_inner_join(df_analysis, df1_status, df2_status):
+    """
+    merge dataframes with status and metrics
+    """
     df1_status = df1_status.merge(df_analysis, on="Problem name", how="inner")
     df2_status = df2_status.merge(df_analysis, on="Problem name", how="inner")
     df1_status.to_csv("status_eps-3.csv")
