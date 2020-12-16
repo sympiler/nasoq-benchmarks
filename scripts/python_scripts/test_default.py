32e1c5f540096d1867a7da6f08372e95e9804377
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2020-12-16 19:22:02 +0800

    update

--- scripts/python_scripts/test_default.py
+++ scripts/python_scripts/test_default.py
@@ -6,9 +6,9 @@ import os
 MAX_TIMING = 1e8
 SOLUTION_PRESENT = [1]
 
-path1 = "test_smp_test_settings_csvs/"
-path2 = "test_smp_tune_csvs/"
-plots_path = "test_smp_opt_test_plots/"
+path1 = "SMP_Repository_test_settings_csvs/"
+path2 = "SMP_Repository_tune_csvs/"
+plots_path = "SMP_Repository_opt_test_plots/"
 
 # recommended settings:
 # diag_perturb = -8, max_iter = 3
@@ -90,10 +90,10 @@ def compute_speedup1():
     eps3_speedup = [speedup["nasoq-fixed_eps-3"], speedup["nasoq-tuned_eps-3"], speedup["nasoq-custom_eps-3"]]
     eps6_speedup = [speedup["nasoq-fixed_eps-6"], speedup["nasoq-tuned_eps-6"], speedup["nasoq-custom_eps-6"]]
 
-    plt.figure()
+    plt.figure(figsize=(10, 10))
     labels = ['nasoq-fixed', 'nasoq-tuned', 'nasoq-custom']
     x = np.arange(3)
-    width = 0.35
+    width = 0.25
 
     fig, ax = plt.subplots()
     rects1 = ax.bar(x - width/2, eps3_speedup, width, label='eps-3')
@@ -106,6 +106,7 @@ def compute_speedup1():
     ax.set_xticklabels(labels)
     ax.legend()
 
+    plt.tight_layout()
     plt.savefig(plots_path + "different_solvers.png")
 
 def compute_speedup2():
@@ -153,7 +154,7 @@ def compute_speedup2():
     max_iter_lst = ["0", "1", "2", "3", "4", "5", "10", "20"]
 
     speedup1 = {"tested-3": 1.0, "tested-6": 1.0}
-    labels1 = ["optimized nasoq-custom", "diag_perturb -12", "diag_perturb -11", "diag_perturb -10", "diag_perturb -9", \
+    labels1 = ["optimized\nnasoq-custom", "diag_perturb -12", "diag_perturb -11", "diag_perturb -10", "diag_perturb -9", \
         "diag_perturb -8", "diag_perturb -7", "diag_perturb -6"]
 
     for datum in data1:
@@ -180,7 +181,7 @@ def compute_speedup2():
                         speedup1["diag_perturb" + diag_perturb + "eps-6"] = ref_gmean["eps-6"] / gmean
     
     speedup2 = {"tested-3": 1.0, "tested-6": 1.0}
-    labels2 = ["optimized nasoq-custom", "max_iter 0", "max_iter 1", "max_iter 2", "max_iter 3", \
+    labels2 = ["optimized\nnasoq-custom", "max_iter 0", "max_iter 1", "max_iter 2", "max_iter 3", \
         "max_iter 4", "max_iter 5", "max_iter 10", "max_iter 20"]
     
     for datum in data2:
@@ -218,9 +219,9 @@ def compute_speedup2():
     eps6_speedup2 = [speedup2["tested-6"], speedup2["max_iter0eps-6"], speedup2["max_iter1eps-6"], speedup2["max_iter2eps-6"], speedup2["max_iter3eps-6"], \
         speedup2["max_iter4eps-6"], speedup2["max_iter5eps-6"], speedup2["max_iter10eps-6"], speedup2["max_iter20eps-6"]]
     
-    plt.figure()
+    plt.figure(figsize=(10, 10))
     x = np.arange(len(labels1))
-    width = 0.35
+    width = 0.25
 
     fig, ax = plt.subplots()
     rects1 = ax.bar(x - width/2, eps3_speedup1, width, label='eps-3')
@@ -233,13 +234,14 @@ def compute_speedup2():
     ax.set_xticklabels(labels1)
     ax.legend()
     for tick in ax.get_xticklabels():
-        tick.set_rotation(20)
+        tick.set_rotation(30)
 
+    plt.tight_layout()
     plt.savefig(plots_path + "different_diag_perturb.png")
 
-    plt.figure()
+    plt.figure(figsize=(10, 10))
     x = np.arange(len(labels2))
-    width = 0.35
+    width = 0.25
 
     fig, ax = plt.subplots()
     rects1 = ax.bar(x - width/2, eps3_speedup2, width, label='eps-3')
@@ -252,11 +254,12 @@ def compute_speedup2():
     ax.set_xticklabels(labels2)
     ax.legend()
     for tick in ax.get_xticklabels():
-        tick.set_rotation(20)
+        tick.set_rotation(30)
 
+    plt.tight_layout()
     plt.savefig(plots_path + "different_max_iter.png")
 
-def computer_performance_profile1():
+def compute_performance_profile1():
     """
     plot the performance profile for NASOQ-CUSTOM optimized version
     and other NASOQ variates with the same settings
@@ -320,7 +323,7 @@ def computer_performance_profile1():
         Plot performance profiles in matplotlib for specified problems and solvers
         """
         df = pd.read_csv(plots_path + "diff_variates_performance_profile_eps{}.csv".format(tol))
-        plt.figure()
+        plt.figure(figsize=(10, 10))
         for solver in solvers:
             plt.plot(df["tau"], df[solver], label=solver)
         plt.xlim(1., 10000.)
@@ -338,7 +341,7 @@ def computer_performance_profile1():
         compute_helper(eps)
         plot_performance_profiles(eps)
 
-def computer_performance_profile2():
+def compute_performance_profile2():
     """
     plot the performance profile for NASOQ-CUSTOM optimized version
     and non-optimized version
@@ -369,7 +372,7 @@ def computer_performance_profile2():
         status["optimized nasoq-custom"] = df["Status"].values
         for idx in range(n_problems):
             if status["optimized nasoq-custom"][idx] not in SOLUTION_PRESENT:
-                t[solver][idx] = MAX_TIMING
+                t["optimized nasoq-custom"][idx] = MAX_TIMING
         
         for solver in solvers[1:]:
             for datum in data2:
@@ -430,7 +433,7 @@ def computer_performance_profile2():
             param = "max_iter"
     
         df = pd.read_csv(plots_path + "{}_performance_profile_eps{}.csv".format(param, tol))
-        plt.figure()
+        plt.figure(figsize=(10, 10))
         for solver in solvers:
             plt.plot(df["tau"], df[solver], label=solver)
         plt.xlim(1., 10000.)
@@ -449,10 +452,206 @@ def computer_performance_profile2():
             compute_helper(eps, solvers)
             plot_performance_profiles(solvers, eps)
 
+def compute_failure_rate2():
+    """
+    failure rate of nasoq-custom with different settings
+    """
+    data = os.listdir(path1)
+    custom_data = []
+    frate = {}
+    for datum in data:
+        df = pd.read_csv(path1 + datum)
+        status = df["Status"].values
+        convergence_lst = []
+        for s in status:
+            if s != 1:
+                convergence_lst.append(0)
+            else:
+                convergence_lst.append(1)
+        failure_rate = 1.0 - convergence_lst.count(1) / len(convergence_lst)
+        if "eps-3" in datum:
+            if "nasoq-custom" in datum:
+                frate["nasoq-custom_eps-3"] = failure_rate
+            elif "nasoq-tuned" in datum:
+                frate["nasoq-tuned_eps-3"] = failure_rate
+            elif "nasoq-fixed" in datum:
+                frate["nasoq-fixed_eps-3"] = failure_rate
+        elif "eps-6" in datum:
+            if "nasoq-custom" in datum:
+                frate["nasoq-custom_eps-6"] = failure_rate
+            elif "nasoq-tuned" in datum:
+                frate["nasoq-tuned_eps-6"] = failure_rate
+            elif "nasoq-fixed" in datum:
+                frate["nasoq-fixed_eps-6"] = failure_rate
+
+    eps3_speedup = [frate["nasoq-fixed_eps-3"], frate["nasoq-tuned_eps-3"], frate["nasoq-custom_eps-3"]]
+    eps6_speedup = [frate["nasoq-fixed_eps-6"], frate["nasoq-tuned_eps-6"], frate["nasoq-custom_eps-6"]]
+
+    plt.figure(figsize=(10, 10))
+    labels = ['nasoq-fixed', 'nasoq-tuned', 'nasoq-custom']
+    x = np.arange(3)
+    width = 0.25
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, np.array(eps3_speedup) * 100, width, label='eps-3')
+    rects2 = ax.bar(x + width/2, np.array(eps6_speedup) * 100, width, label='eps-6')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('failure rate * 100')
+    ax.set_title('failure rate * 100 of three tools with recommended settings')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(30)
+
+    plt.tight_layout()
+    plt.savefig(plots_path + "different_solvers_frate.png")
+
+def compute_failure_rate1():
+    """
+    failure rate of three tools with the same settings
+    """
+    data = os.listdir(path1)
+    custom_data = []
+    for datum in data:
+        if "nasoq-custom" in datum:
+            custom_data.append(datum)
+    data1 = os.listdir(path2 + "diag_perturb/")
+    data2 = os.listdir(path2 + "max_iter/")
+    
+    diag_perturb_lst = ["-12", "-11", "-10", "-9", "-8", "-7", "-6"]
+    max_iter_lst = ["0", "1", "2", "3", "4", "5", "10", "20"]
+
+    frate1 = {}
+    frate2 = {}
+
+    labels1 = ["optimized\nnasoq-custom", "diag_perturb -12", "diag_perturb -11", "diag_perturb -10", "diag_perturb -9", \
+        "diag_perturb -8", "diag_perturb -7", "diag_perturb -6"]
+    
+    for datum in custom_data:
+        df = pd.read_csv(path1 + datum)
+        status = df["Status"].values
+        convergence_lst = []
+        for s in status:
+            if s != 1:
+                convergence_lst.append(0)
+            else:
+                convergence_lst.append(1)
+        failure_rate = 1.0 - convergence_lst.count(1) / len(convergence_lst)
+
+        if "eps-3" in datum:
+            frate1["tested-3"] = failure_rate
+            frate2["tested-3"] = failure_rate
+        elif "eps-6" in datum:
+            frate1["tested-6"] = failure_rate
+            frate2["tested-6"] = failure_rate
+
+    for datum in data1:
+        if "nasoq-custom" in datum:
+            df = pd.read_csv(path2 + "diag_perturb/" + datum)
+            status = df["Status"].values
+            convergence_lst = []
+            for s in status:
+                if s != 1:
+                    convergence_lst.append(0)
+                else:
+                    convergence_lst.append(1)
+            failure_rate = 1.0 - convergence_lst.count(1) / len(convergence_lst)
+
+            if "eps-3" in datum:
+                for diag_perturb in diag_perturb_lst:
+                    if diag_perturb in datum and diag_perturb + "0" not in datum:
+                        frate1["diag_perturb" + diag_perturb + "eps-3"] = failure_rate
+            
+            elif "eps-6" in datum:
+                for diag_perturb in diag_perturb_lst:
+                    if diag_perturb in datum and diag_perturb + "0" not in datum:
+                        frate1["diag_perturb" + diag_perturb + "eps-6"] = failure_rate
+    
+    labels2 = ["optimized\nnasoq-custom", "max_iter 0", "max_iter 1", "max_iter 2", "max_iter 3", \
+        "max_iter 4", "max_iter 5", "max_iter 10", "max_iter 20"]
+    
+    for datum in data2:
+        if "nasoq-custom" in datum:
+            df = pd.read_csv(path2 + "max_iter/" + datum)
+            status = df["Status"].values
+            convergence_lst = []
+            for s in status:
+                if s != 1:
+                    convergence_lst.append(0)
+                else:
+                    convergence_lst.append(1)
+            failure_rate = 1.0 - convergence_lst.count(1) / len(convergence_lst)
+            if "eps-3" in datum:
+                for max_iter in max_iter_lst:
+                    if max_iter in datum and max_iter + "0" not in datum:
+                        frate2["max_iter" + max_iter + "eps-3"] = failure_rate
+            
+            elif "eps-6" in datum:
+                for max_iter in max_iter_lst:
+                    if max_iter in datum and max_iter + "0" not in datum:
+                        frate2["max_iter" + max_iter + "eps-6"] = failure_rate
+    
+    eps3_frate1 = [frate1["tested-3"], frate1["diag_perturb-12eps-3"], frate1["diag_perturb-11eps-3"], frate1["diag_perturb-10eps-3"], \
+        frate1["diag_perturb-9eps-3"], frate1["diag_perturb-8eps-3"], frate1["diag_perturb-7eps-3"], frate1["diag_perturb-6eps-3"]]
+
+    eps6_frate1 = [frate1["tested-6"], frate1["diag_perturb-12eps-6"], frate1["diag_perturb-11eps-6"], frate1["diag_perturb-10eps-6"], \
+        frate1["diag_perturb-9eps-6"], frate1["diag_perturb-8eps-6"], frate1["diag_perturb-7eps-6"], frate1["diag_perturb-6eps-6"]]
+    
+    eps3_frate2 = [frate2["tested-3"], frate2["max_iter0eps-3"], frate2["max_iter1eps-3"], frate2["max_iter2eps-3"], frate2["max_iter3eps-3"], \
+        frate2["max_iter4eps-3"], frate2["max_iter5eps-3"], frate2["max_iter10eps-3"], frate2["max_iter20eps-3"]]
+    
+    eps6_frate2 = [frate2["tested-6"], frate2["max_iter0eps-6"], frate2["max_iter1eps-6"], frate2["max_iter2eps-6"], frate2["max_iter3eps-6"], \
+        frate2["max_iter4eps-6"], frate2["max_iter5eps-6"], frate2["max_iter10eps-6"], frate2["max_iter20eps-6"]]
+    
+    plt.figure(figsize=(15, 15))
+    x = np.arange(len(labels1))
+    width = 0.25
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, np.array(eps3_frate1) * 100, width, label='eps-3')
+    rects2 = ax.bar(x + width/2, np.array(eps6_frate1) * 100, width, label='eps-6')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('failure rate * 100')
+    ax.set_title('failure rate * 100 of nasoq-custom \n(optimized vs non-optimized) for diag_perturb')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels1)
+    ax.legend()
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(30)
+    plt.gcf().subplots_adjust(bottom=0.2)
+
+    plt.savefig(plots_path + "different_diag_perturb_frate.png")
+
+    plt.figure(figsize=(10, 10))
+    x = np.arange(len(labels2))
+    width = 0.25
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - width/2, np.array(eps3_frate2) * 100, width, label='eps-3')
+    rects2 = ax.bar(x + width/2, np.array(eps6_frate2) * 100, width, label='eps-6')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('failure rate * 100')
+    ax.set_title('failure rate * 100 of nasoq-custom \n(optimized vs non-optimized) for max_iter')
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels2)
+    ax.legend()
+    for tick in ax.get_xticklabels():
+        tick.set_rotation(30)
+
+    plt.tight_layout()
+    plt.savefig(plots_path + "different_max_iter_frate.png")
+
+
 if __name__ == "__main__":
     if not os.path.exists(plots_path):
         os.makedirs(plots_path)
     compute_speedup1()
     compute_speedup2()
-    computer_performance_profile1()
-    computer_performance_profile2()
+    compute_performance_profile1()
+    compute_performance_profile2()
+    compute_failure_rate1()
+    compute_failure_rate2()
