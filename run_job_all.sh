c63a64e30896fb6a33e0f6ae19c63e56a8c3214b
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2021-01-13 13:04:50 +0800

    add more

--- run_job_all.sh
+++ run_job_all.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-#SBATCH --mem=80GB
+#SBATCH --job-name="NASOQ_"
+#SBATCH --output="NASOQ.%j.%N.out"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --export=ALL
+#SBATCH -t 23:59:00
+#SBATCH --mem=40GB
+#SBATCH --mail-type=begin  # email me when the job starts
+#SBATCH --mail-type=end    # email me when the job finishes
+export OMP_NUM_THREADS=8
 
 # load all modules
 module load cmake
