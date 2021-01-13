ca0e9b0333a7525710f589a2437f59f3f7a1a85e
JunmingZhang <junmingpeter.zhang@mail.utoronto.ca>
2021-01-13 13:01:19 +0800

    plot on computing node

---
+++ all_plot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+var="$(python -c 'import sys; print(sys.version_info[0])')"
+if [[ $var == 2 ]]; then
+    python3 all.py logs/perf_data
+else
+    python all.py logs/perf_data
+fi
--- run_job_all.sh
+++ run_job_all.sh
@@ -10,10 +10,4 @@ module load scipy-stack
 
 sbatch all.sh ../SMP_Repository $MKLROOT /home/zjming1/metis-5.1.0/
 sbatch all.sh ../SMP_Repository2 $MKLROOT /home/zjming1/metis-5.1.0/
-
-var="$(python -c 'import sys; print(sys.version_info[0])')"
-if [[ $var == 2 ]]; then
-    python3 all.py logs/perf_data
-else
-    python all.py logs/perf_data
-fi
+sbatch all_plot.sh
